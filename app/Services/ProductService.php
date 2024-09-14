dd7c5e9562843d503108ac0f2b3f59507365e601
Zakaria Bounou <zakariabounouu@gmail.com>
2024-09-14 10:34:33 +0100

    update readme + service creation

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -7,14 +7,4 @@ use App\Models\Product;
 
 class ProductService
 {
-    public function createProduct($data)
-    {
-        $product = new Product();
-        $product->name = $data['name'];
-        $product->description = $data['description'];
-        $product->price = $data['price'];
-        $product->save();
-
-        return $product;
-    }
 }
